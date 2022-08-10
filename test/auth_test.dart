46d8217af5657100890222e5426166e12593a179
wedad <201904162@bethlehem.edu>
2022-08-10 21:24:11 +0300

    Updating Existing Notes

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,7 +1,7 @@
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Mock Authentication', () {
